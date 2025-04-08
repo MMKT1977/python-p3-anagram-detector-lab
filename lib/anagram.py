53b516a3c6271bfef3614f67ff899a0c656b2e30
MMKT1977 <kennedy.moranga@student.moringaschool..com>
2025-04-08 15:20:48 +0300

    anagram  match class

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, my_anagrams):
+        matches =[]
+        sorted_word = sorted(self.word)
+
+        for match in my_anagrams:
+            if sorted(match.lower())== sorted_word:
+
+                matches.append(match)
+
+        return matches
+
